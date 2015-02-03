4b334d7a0fe98026e47aa2ff3a158d574b7936a3
Sriram <sri.kmb@gmail.com>
2015-02-04 00:18:32 +0530

    - Changed the javaee api dependency scope from test to default. - Replaced the testNg assert with junit assert statement.

--- cdi-unit-tests-ejb/src/test/java/org/junit/cdiunit/MyBeanUnitTest.java
+++ cdi-unit-tests-ejb/src/test/java/org/junit/cdiunit/MyBeanUnitTest.java
@@ -3,16 +3,15 @@ package org.junit.cdiunit;
 import org.jglue.cdiunit.AdditionalClasses;
 import org.jglue.cdiunit.CdiRunner;
 import org.jglue.cdiunit.ejb.SupportEjb;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.testng.Assert;
 
 import javax.ejb.EJB;
 
 /**
  * Unit Test for the EJB singleton bean
  */
-
 @SupportEjb
 @RunWith(CdiRunner.class)
 @AdditionalClasses(MyBeanImpl.class)
